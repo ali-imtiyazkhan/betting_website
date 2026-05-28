0b1a2d73bdb97d86dc5784d1396e20144a61fa2e
Imtiyaz A. Khan <itzkhantijara@gmail.com>
2026-05-28 15:18:34 +0530

    fix:backend code is fixed and add some extra additional code here

---
+++ apps/backend/types.ts
@@ -0,0 +1,27 @@
+import z from "zod";
+
+export const CreateOrderSchema = z.object({
+    marketId: z.string(),
+    side: z.enum(["yes", "no"]),
+    type: z.enum(["buy", "sell"]),
+    price: z.int(),
+    qty: z.int()
+})
+
+export type Orderbook = {[key: string]: {
+    availableQty: number,
+    orders: { userId: string, qty: number, filledQty: number, originalOrderId: string, reverseOrder: boolean }[]
+}}
+
+export const SplitSchema = z.object({
+    marketId: z.string(),
+    amount: z.number()
+})
+
+export const OnrampSchema = z.object({
+    amount: z.number()
+})
+
+export const OfframpSchema = z.object({
+    amount: z.number()
+})
